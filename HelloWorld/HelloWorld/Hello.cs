84eebff6c8caaa488a860251b66f9d47f16d5085
Nathan Plymale <ndplymale@gmail.com>
2017-08-09 13:44:03 -0400

    First Commit.

---
+++ HelloWorld/HelloWorld/Hello.cs
@@ -0,0 +1,18 @@
+﻿/* A "Hello World!" program in C#.
+ * This program displays the string "Hello World!" on the screen. */
+
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
